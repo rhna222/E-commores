f7dd83a4fc820784ac34cb3d27f00ef9af4610df
rhna222 <rahulhna02@gmail.com>
2022-08-05 00:37:01 +0530

    new

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5h7$b0j^97qu67_8bz-^%2r3fd8gr#62=7fq6eha!dhlp)=2%w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
